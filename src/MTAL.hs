d809e3dc53d6e5db3e0a6fc7a773d256360723be
elpinal <elpinal@users.noreply.github.com>
2018-10-18 06:52:17 +0900

    Support type abstraction

--- src/MTAL.hs
+++ src/MTAL.hs
@@ -2,29 +2,72 @@
 {-# LANGUAGE TypeFamilyDependencies #-}
 
 module MTAL
-  (
+  ( ObjectFile(..)
+  , Heap(..)
+  , Types(..)
+  , TypeDecl
+  , Interface
+  , KT(..)
+  , wf
+  , compatible
+  , link
+  , produce
+  , executable
+  , exec
+  , Label(..)
+  , File(..)
+  , Inst(..)
   ) where
 
-import Data.Monoid
 import qualified Data.Map.Lazy as Map
+import Data.Monoid
 import qualified Data.Set as Set
 
 class Types h => Heap h where
-  type Type h = t | t -> h
-  labels :: h -> Set.Set Label
-  usedLabels :: h -> Set.Set Label
-  -- | Merges two disjoint heaps.
+  -- |
+  -- Merges two heaps.
+  -- Alpha-conversion of local labels is required to avoid name clashes.
   merge :: h -> h -> h
 
-  assumption :: h -> Map.Map Label (Type h)
-  typeOf :: Map.Map Label (Type h) -> h -> Bool
+  typeOf :: Map.Map Label (Kind h) -> Map.Map Label (Type h) -> Map.Map Label (Type h) -> h -> Bool
 
   entryConform :: h -> Label -> Bool
 
+  typeDecl :: h -> TypeDecl h
+  interfaceOf :: h -> Interface h
+
+  labels :: h -> Set.Set Label
+  labels = Map.keysSet . interfaceOf
+
+type TypeDecl h = Map.Map Label (Kind h, Type h)
+
+data KT h
+  = K (Kind h)
+  | T (Type h)
+
+getK :: Interface h -> Map.Map Label (Kind h)
+getK = Map.mapMaybe f
+  where
+    f (K x) = Just x
+    f _ = Nothing
+
+getT :: Interface h -> Map.Map Label (Type h)
+getT = Map.mapMaybe f
+  where
+    f (T x) = Just x
+    f _ = Nothing
+
+type Interface h = Map.Map Label (KT h)
+
 class Types h where
+  type Type h = t | t -> h
+  type Kind h = k | k -> h
+
   equal :: Type h -> Type h -> Bool
-  isSubinterfaceOf :: Map.Map Label (Type h) -> Map.Map Label (Type h) -> Bool
-  wfInterface :: Map.Map Label (Type h) -> Bool
+  equalK :: Kind h -> Kind h -> Bool
+
+  wfHeap :: Map.Map Label (Kind h) -> Map.Map Label (Type h) -> Bool
+  kindOf :: Map.Map Label (Kind h) -> Type h -> Kind h -> Bool
 
 dom :: Heap h => h -> Set.Set Label
 dom = labels
@@ -40,21 +83,35 @@ newtype Label = Label String
 
 data ObjectFile h = ObjectFile
   { heap :: h
-  , imports :: Map.Map Label (Type h)
-  , exports :: Map.Map Label (Type h)
+  , imports :: Interface h
+  , exports :: Interface h
   }
 
--- Determines well-formedness.
+kinding :: Types h => Map.Map Label (Kind h) -> TypeDecl h -> Bool
+kinding m i = getAll $ foldMap (\(k, t) -> All $ kindOf m t k) $ Map.elems i
+
+isSubKindEnvOf :: Types h => Map.Map Label (Kind h) -> Map.Map Label (Kind h) -> Bool
+isSubKindEnvOf = flip $ Map.isSubmapOfBy equalK
+
+isSubinterfaceOf :: Heap h => Interface h -> Interface h -> Bool
+isSubinterfaceOf i1 i2 = (getK i1 `isSubKindEnvOf` getK i2) && Map.isSubmapOfBy equal (getT i2) (getT i1)
+
+-- | Determines well-formedness.
 wf :: Heap h => ObjectFile h -> Bool
 wf o = and
-  [ wfInterface $ imports o
-  , assumption (heap o) `isSubinterfaceOf` exports o
+  [ interfaceOf (heap o) `isSubinterfaceOf` exports o
   , Map.keysSet (imports o) `Set.disjoint` dom (heap o)
-  , typeOf (imports o `Map.union` assumption (heap o)) (heap o)
+  , kinding (fmap fst $ typeDecl $ heap o) $ typeDecl $ heap o
   ]
 
-compatibleInterface :: Types h => Map.Map Label (Type h) -> Map.Map Label (Type h) -> Bool
-compatibleInterface i1 i2 = getAll $ foldMap (All . uncurry equal) $ Map.intersectionWith (,) i1 i2
+compatibleInterface :: Types h => Interface h -> Interface h -> Bool
+compatibleInterface i1 i2 = and
+  [ getAll $ foldMap f $ Map.intersectionWith (,) (getK i1) (getK i2)
+  , getAll $ foldMap g $ Map.intersectionWith (,) (getT i1) (getT i2)
+  ]
+  where
+    f (k1, k2) = All $ equalK k1 k2
+    g (t1, t2) = All $ equal t1 t2
 
 -- Given two well formed object files, `compatible o1 o2` returns whether they
 -- are link compatible or not.
@@ -78,7 +135,12 @@ link o1 o2 =
     else Nothing
 
 executable :: Heap h => h -> Label -> Bool
-executable h entry = wfInterface (assumption h) && typeOf (assumption h) h && entryConform h entry
+executable h entry = and
+  [ wfHeap (getK $ interfaceOf h) (getT $ interfaceOf h)
+  , kinding (fmap fst $ typeDecl h) (typeDecl h)
+  , typeOf (fst <$> typeDecl h) (snd <$> typeDecl h) (getT $ interfaceOf h) h
+  , entryConform h entry
+  ]
 
 -- Produces a well-formed executable (really?).
 produce :: Heap h => ObjectFile h -> Label -> Maybe (h, Label)
