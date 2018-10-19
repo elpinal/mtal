32acce3dd4a5909ace072c3a1e970d87fec42ada
elpinal <elpinal@users.noreply.github.com>
2018-10-19 20:44:34 +0900

    Add some test cases

--- test/TALSpec.hs
+++ test/TALSpec.hs
@@ -73,3 +73,93 @@ spec = do
           , typeDeclH = mempty
           }
         } `shouldBe` False
+
+      wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.K ()
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [Mov (Reg 0) $ Register $ Reg 1] $ Value $ Label $ M.Label "a"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.fromList [(Reg 0, TLabel $ M.Label "i"), (Reg 1, TLabel $ M.Label "i")]
+          , typeDeclH = mempty
+          }
+        }
+
+      wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T $ Code $ Context mempty
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [Mov (Reg 0) $ Register $ Reg 1] $ Value $ Label $ M.Label "a"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.fromList [(Reg 0, Int), (Reg 1, Int)]
+          , typeDeclH = mempty
+          }
+        }
+
+      wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T Int -- A controversial case.
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [Mov (Reg 0) $ Register $ Reg 1] $ Value $ Label $ M.Label "a"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.fromList [(Reg 0, Int), (Reg 1, Int)]
+          , typeDeclH = mempty
+          }
+        }
+
+      wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T $ Code $ Context $ Map.fromList [(Reg 0, Int), (Reg 1, Int)]
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [Mov (Reg 0) $ Register $ Reg 1] $ Value $ Label $ M.Label "i"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.singleton (Reg 1) Int
+          , typeDeclH = mempty
+          }
+        }
+
+      M.wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T $ Code $ Context $ Map.fromList [(Reg 0, Int), (Reg 1, Int), (Reg 3, Int)]
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [Mov (Reg 0) $ Register $ Reg 1] $ Value $ Label $ M.Label "i"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.singleton (Reg 1) Int
+          , typeDeclH = mempty
+          }
+        } `shouldBe` False
+
+      wf M.ObjectFile
+        { M.imports = mempty
+        , M.exports = Map.singleton (M.Label "a") $ M.T $ Code $ Context $ mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [] $ Value $ Label $ M.Label "a"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ mempty
+          , typeDeclH = mempty
+          }
+        }
+
+      M.wf M.ObjectFile
+        { M.imports = mempty
+        , M.exports = Map.singleton (M.Label "a") $ M.T $ Code $ Context $ Map.singleton (Reg 20) Int
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [] $ Value $ Label $ M.Label "a"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ mempty
+          , typeDeclH = mempty
+          }
+        } `shouldBe` False
+
+      wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T $ Code $ Context $ Map.singleton (Reg 0) Int
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [] $ Value $ Label $ M.Label "i"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.singleton (Reg 0) Int
+          , typeDeclH = Map.singleton (M.Label "t") ((), Int)
+          }
+        }
+
+      M.wf M.ObjectFile
+        { M.imports = Map.singleton (M.Label "i") $ M.T $ Code $ Context $ Map.singleton (Reg 0) Int
+        , M.exports = mempty
+        , M.heap = H
+          { heapH = Heap $ Map.singleton (M.Label "a") $ Block [] $ Value $ Label $ M.Label "i"
+          , heapContextH = HeapContext $ Map.singleton (M.Label "a") $ Code $ Context $ Map.singleton (Reg 0) $ TLabel $ M.Label "t"
+          , typeDeclH = Map.singleton (M.Label "t") ((), Int)
+          }
+        } `shouldBe` False
